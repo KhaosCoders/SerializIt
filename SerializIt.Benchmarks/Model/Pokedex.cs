ad5a5b6766b2ac92f531012fcdc337bc056d1018
Khaos <khaos@khaos-coders.org>
2022-02-18 15:17:46 +0100

    Benchmark project (JSON)

---
+++ SerializIt.Benchmarks/Model/Pokedex.cs
@@ -0,0 +1,40 @@
+﻿namespace SerializIt.Benchmarks.Model
+{
+    public class Pokedex
+    {
+        public Pokemon[] Pokemon { get; set; }
+    }
+
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Num { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string[] Type { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Candy { get; set; }
+        public int Candy_count { get; set; }
+        public string Egg { get; set; }
+        public float Spawn_chance { get; set; }
+        public float Avg_spawns { get; set; }
+        public string Spawn_time { get; set; }
+        public float[] Multipliers { get; set; }
+        public string[] Weaknesses { get; set; }
+        public Next_Evolution[] Next_evolution { get; set; }
+        public Prev_Evolution[] Prev_evolution { get; set; }
+    }
+
+    public class Next_Evolution
+    {
+        public string Num { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Prev_Evolution
+    {
+        public string Num { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SerializIt.Benchmarks/PokedexBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using SerializIt.Benchmarks.Model;
+using System.Text.Json;
+
+namespace SerializIt.Benchmarks;
+
+public class PokedexBenchmark
+{
+    private readonly Pokedex _pokedex;
+
+    public PokedexBenchmark()
+    {
+        string data = File.ReadAllText(@"Data\pokedex.json");
+        System.Text.Json.JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        _pokedex = JsonSerializer.Deserialize<Pokedex>(data, options) ?? new();
+    }
+
+    [Benchmark]
+    public string SerializIt()
+    {
+        PokedexJsonContext ctx = new();
+        return ctx.Pokedex.SerializeDocument(_pokedex);
+    }
+}
