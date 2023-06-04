d4db717b96f7ba7cef60ba057f6b5b75c1fe982c
Khaos <khaos@khaos-coders.org>
2023-06-04 16:07:19 +0200

    Switched from CSharpCompliation to CSharpScript

--- SerializIt.Generator/Helpers/CodeActivator.cs
+++ SerializIt.Generator/Helpers/CodeActivator.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Loader;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace SerializIt.Generator.Helpers;
 
 public static class CodeActivator
 {
-    private static readonly AssemblyLoadContext assemblyLoadContext = new GeneratorAssemblyLoadContext();
-
     public static T? Attribute<T>(string code) where T : Attribute
     {
         if (string.IsNullOrWhiteSpace(code))
@@ -20,27 +18,7 @@ public static class CodeActivator
             return default;
         }
 
-        // Ensure attribute syntax
-        if (code[0] != '[')
-        {
-            code = $"[{code.Replace("Attribute(", "(")}]";
-        }
-
-        // compile dummy class with attribute
-        var objWithAttr = Class<object>($"namespace Generated {{ {code} public class Dummy {{ }} }}", "Generated.Dummy");
-
-        // get dummy class type and extract attribute
-        var objType = objWithAttr?.GetType();
-        return objType?.GetCustomAttributes(false)[0] as T;
-    }
-
-    public static T? Class<T>(string code, string fullName) where T : class
-    {
-        if (string.IsNullOrWhiteSpace(code))
-        {
-            return default;
-        }
-
+        // Referenced assemblies
         List<MetadataReference> references = new();
         references.AddMetadataRef(typeof(ESerializers));
         references.AddMetadataRef(typeof(object));
@@ -51,42 +29,37 @@ public static class CodeActivator
         references.AddMetadataRef("netstandard");
         references.AddMetadataRef("System.Runtime");
 
-        // compile class as dll
-        var assemblyName = Path.GetRandomFileName();
-        var syntaxTree = CSharpSyntaxTree.ParseText(code);
-        var compilation = CSharpCompilation.Create(assemblyName,
-                                         new[] { syntaxTree },
-                                                   references,
-                                                   new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+        // ScriptOptions with IsCollectible=true
+        var options = ScriptOptions.Default
+            .WithReferences(references);
+        var isCollectableMeth = typeof(ScriptOptions).GetMethod("WithIsCollectible", BindingFlags.Instance | BindingFlags.NonPublic);
+        isCollectableMeth?.Invoke(options, new object[] { true });
 
-        // emit bits to MemoryStream
-        using var ms = new MemoryStream();
-        var result = compilation.Emit(ms);
+        // Create Script
+        var script = CSharpScript.Create($"return new {code};", options);
 
-        if (!result.Success)
+        // Check for errors in script
+        var diagnostics = script.Compile();
+        var failures = diagnostics.Where(diagnostic =>
+                diagnostic.IsWarningAsError ||
+                diagnostic.Severity == DiagnosticSeverity.Error);
+
+        if (failures.Any())
         {
-            // compilation failed
-            var failures = result.Diagnostics.Where(diagnostic =>
-                    diagnostic.IsWarningAsError ||
-                    diagnostic.Severity == DiagnosticSeverity.Error);
-
             foreach (var diagnostic in failures)
             {
                 Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
             }
-        }
-        else
-        {
-            // load assembly from memory
-            ms.Seek(0, SeekOrigin.Begin);
-            var assembly = assemblyLoadContext.LoadFromStream(ms);
-
-            // create an instance for the compiled type
-            var type = assembly.GetType(fullName);
-            return Activator.CreateInstance(type) as T;
+            return default;
         }
 
-        return default;
+        // Run script
+        var task = script.RunAsync();
+        task.Wait();
+
+        // Return result
+        var state = task.Result;
+        return state.ReturnValue as T;
     }
 
     internal static void AddMetadataRef(this List<MetadataReference> list, Type type) =>
